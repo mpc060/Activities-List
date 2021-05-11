0764e360bb8b148f66a8bba74ed5d3e0e64d24f0
Marcelo <mpc_musicmachine@hotmail.com>
2021-05-10 21:15:06 -0300

    Create project

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+    <h2>Nossa História</h2>
+    <p>Jazz Bass is created by Fender</p>
+    <h2>Missão e Visão</h2>
+    <p>Jazz Bass is a excelent instrument</p>
+    <h2>Imprensa</h2>
+    <p>12 Jazz Corp.</p>
+  </div>
+)
